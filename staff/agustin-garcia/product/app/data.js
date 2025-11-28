b6681af1fcb675f84765a2ee2f9f299fc282e3af
Agustin.dev <Agustin.tech@proton.me>
2025-11-28 15:54:12 +0100

    add changes to all files js

--- staff/agustin-garcia/product/app/data.js
+++ staff/agustin-garcia/product/app/data.js
@@ -1,6 +1,6 @@
 // models
 
-function User(id, name, surname, username, password, role) {
+function User(id, name, email, username, password, role) {
     this.id = id
     this.name = name
     this.email = email
@@ -8,6 +8,19 @@ function User(id, name, surname, username, password, role) {
     this.password = password
     this.role = role
 }
+
+
+function Owner(passport, name, surname, address, phone, email) {
+    this.id = id
+    this.passport = passport
+    this.name = name
+    this.surname = surname
+    this.address = address
+    this.phone = phone
+    this.email = email
+    this.active = true
+}
+
 
 function Pet(id, userId, chip, name, gender, birthdate, species, race, colors) {
     this.id = id
@@ -21,24 +34,132 @@ function Pet(id, userId, chip, name, gender, birthdate, species, race, colors) {
     this.colors = colors
 }
 
+function Vaccine(id, name, description) {
+    this.id = id
+    this.name = name
+    this.description = description
+}
+
+
 // manager
 
 function Data() {
     this.users = []
     this.userCount = 0
+    this.owners = []
+    this.ownersCount = 0
     this.pets = []
     this.petsCount = 0
+    this.vaccines = []
 }
 
+// users
 Data.prototype.insertUser = function (user) {
     this.users.push(user)
     this.userCount++
 }
 
+Data.prototype.findUserByEmail = function (email) {
+    for (let i = 0; i < this.users.length; i++) {
+        const user = this.users[i]
+
+        if (user.email === email)
+            return email
+    }
+
+    return null
+}
+Data.prototype.findUserByUsername = function (username) {
+    for (let i = 0; i < this.users.length; i++) {
+        const user = this.users[i]
+
+        if (user.username === username)
+            return user
+    }
+
+    return null
+}
+
+
+// owner 
+
+Data.prototype.insertOwner = function (owner) {
+    this.owners.push(owner)
+    this.ownersCount++
+}
+
+Data.prototype.findOwnerById = function (ownerId) {
+    for (let i = 0; i < this.owners.length; i++) {
+        const owner = this.owners[i]
+
+        if (owner.id === ownerId)
+            return owner
+    }
+
+    return null
+}
+
+Data.prototype.findOwnerByPassport = function (ownerPassport) {
+    for (let i = 0; i < this.owners.length; i++) {
+        const owner = this.owners[i]
+
+        if (owner.passport === ownerPassport)
+            return owner
+    }
+
+    return null
+}
+// pets
 Data.prototype.insertPet = function (pet) {
     this.pets.push(pet)
     this.petsCount++
 }
 
+Data.prototype.findPetsByOwnerPassport = function (passport) {
+    const foundPets = []
+
+    for (let i = 0; i < this.pets.length; i++) {
+        const pet = this.pets[i]
+
+        if (pet.ownerId === passport)
+            foundPets.push(pet)
+    }
+
+    return foundPets
+}
+
+
+Data.prototype.findPetById = function (petId) {
+    for (let i = 0; i < this.pets.length; i++) {
+        const pet = this.pets[i]
+
+        if (pet.id === petId)
+            return pet
+    }
+
+    return null
+}
+
+Data.prototype.findPetByPassport = function (passport) {
+    for (let i = 0; i < this.pets.length; i++) {
+        const pet = this.pets[i]
+
+        if (pet.passport === passport)
+            return pet
+    }
+
+    return null
+}
+
+// vaccine 
+Data.prototype.insertVaccine = function (vaccine) {
+    this.vaccines.push(vaccine)
+}
+
+
+
+function Logic() {
+
+}
 // instance
 const data = new Data()
--- staff/agustin-garcia/product/app/logic.js
+++ staff/agustin-garcia/product/app/logic.js
@@ -2,10 +2,125 @@ function Logic() {
 
 }
 
-Logic.prototype.registreUser = function (name, email, username, password, passwordRepeat) {
+Logic.prototype.registerUser = function (name, email, username, password, passwordRepeat) {
     // rules
+
+    if (typeof name !== 'string') throw new Error('invalid name type')
+    if (name.length < 1) throw new Error('invalid name length')
+
+    if (typeof email !== 'string') throw new Error('invalid email type')
+    if (email.length < 6) throw new Error('invalid email length')
+
+    if (typeof username !== 'string') throw new Error('invalid username type')
+    if (username.length < 4) throw new Error('invalid username length')
+
+    if (typeof password !== 'string') throw new Error('invalid password type')
+    if (password.length < 8) throw new Error('invalid password length')
+
+    if (typeof passwordRepeat !== 'string') throw new Error('invalid passwordRepeat type')
+    if (passwordRepeat.length < 8) throw new Error('invalid passwordRepeat length')
+
+    if (password !== passwordRepeat) throw new Error('passwords do not match')
+
+    let user = data.findUserByEmail(email)
+
+    if (user !== null) throw new Error('user email already exists')
+
+    user = data.findUserByUsername(username)
+
+    if (user !== null) throw new Error('user username already exists')
+
+    user = new User('user-' + data.usersCount, name, email, username, password, 'regular')
+
+    data.insertUser(user)
 }
 
+
+Logic.prototype.loginUser = function (username, password) {
+
+    if (typeof username !== 'string') throw new Error('invalid username type')
+    if (username.length < 3) throw new Error('invalid username length')
+
+    if (typeof password !== 'string') throw new Error('invalid password type')
+    if (password.length < 8) throw new Error('invalid password length')
+
+    const user = data.findUserByUsername(username)
+
+    if (user === null) throw new Error('user does not exists')
+
+    if (user.password !== password) throw new Error('invalid password')
+
+}
+
+
+Logic.prototype.registerOwner = function (passport, name, surname, address, phone, email) {
+    if (typeof passport !== 'string') throw new Error('invalid passport type')
+    if (passport.length < 6) throw new Error('invalid passport lenght')
+
+    if (typeof name !== 'string') throw new Error('invalid name type')
+    if (name.length < 1) throw new Error('invalid name length')
+
+    if (typeof surname !== 'string') new Error('invalid surname type')
+    if (surname.length < 2) new Error('invalid surname length')
+
+    if (typeof address !== 'string') new Error('invalid address type')
+    if (address.length < 5) new Error('invalid address length')
+
+    if (typeof phone !== 'string') new Error('invalid phone type')
+    if (phone.length < 5) new Error('invalid phone length')
+
+    if (typeof email !== 'string') new Error('invalid email type')
+    if (email.length < 6) new Error('invalid email length')
+
+    let owner = data.findOwnerByPassport(passport)
+
+    if (owner == null) throw new Error('owner already exists')
+
+    owner = new Owner('owner-' + data.ownersCount, name, surname, address, phone, email)
+
+    data.insertOwner(owner)
+}
+
+Logic.prototype.registerPet = function (id, userId, chip, name, gender, birthdate, species, race, colors) {
+    if (typeof id !== 'string') throw new Error('invalid id type')
+    if (id.length < 5) throw new Error('invalid id length')
+
+    if (typeof userId !== 'string') throw new Error('invalid userId type')
+    if (userId.length < 7) throw new Error('invalid userId length')
+
+    if (typeof chip !== 'string') throw new Error('invalid chip type')
+    if (chip.length < 5) throw new Error('invalid chip length')
+
+    if (typeof name !== 'stirng') throw new Error('invalid name type')
+    if (name.length < 2) throw new Error('invalid name length')
+
+    if (typeof gender !== 'string') throw new Error('invalid gender type')
+    if (gender !== 'female' && gender !== 'male') throw new Error('invalid gender value')
+
+    if (typeof birthdate !== 'number') throw new Error('invalid birthdate type')
+    if (birthdate < 1900) throw new Error('invalid birthdate value')
+
+    if (typeof species !== 'string') throw new Error('invalid species type')
+    if (species.length < 3) throw new Error('invalid species length')
+
+    if (typeof race !== 'string') throw new Error('invalid race type')
+    if (race.length < 3) throw new Error('invalid race length')
+
+    if (typeof colors !== 'string') throw new Error('invalid colors type')
+    if (colors.length < 1) throw new Error('invalid colors length')
+
+    const owner = data.findOwnerByPassport(passport)
+
+    if (owner === null) throw new Error('owner of pet not found')
+
+    let pet = data.findPetByPassport(passport)
+
+    if (pet !== null) throw new Error('pet already exists')
+
+    pet = new Pet('pet-' + data.petsCount, userId, chip, name, gender, birthdate, species, race, colors)
+
+    data.insertPet(pet)
+}
 // instance
 
 const logic = new Logic()
