642e15e48bf09db680e2516cb0087d12d5d8e9c9
Agustin.dev <Agustin.tech@proton.me>
2026-01-21 20:11:00 +0100

    update app to use && operator for views and map arrays in pet-list

--- staff/agustin-garcia/product/app/App.jsx
+++ staff/agustin-garcia/product/app/App.jsx
@@ -19,21 +19,19 @@ function App() {
 
     console.log('App -> render')
 
-    if (view === 'landing')
-        return <Landing onGoToLogin={handleGoToLogin} onGoToRegister={handleGoToRegister} />
+    return <>
 
-    if (view === 'login')
-        return <Login onGoToHome={handleLogin} onGoToRegister={handleGoToRegister} />
+        {view === 'landing' && <Landing onGoToLogin={handleGoToLogin} onGoToRegister={handleGoToRegister} />}
 
-    if (view === 'register')
-        return <Register onGoToLogin={handleGoToLogin} />
+        {view === 'login' && <Login onGoToHome={handleLogin} onGoToRegister={handleGoToRegister} />}
 
-    if (view === 'home')
-        return <Home onGoToAddPet={handleGoToAddPet} onGoToLogin={handleGoToLogin} onGoToProfile={handleGoToProfile} />
+        {view === 'register' && <Register onGoToLogin={handleGoToLogin} />}
 
-    if (view === 'add-pet')
-        return <AddPet onGoToHome={handleGoToHome} />
+        {view === 'home' && <Home onGoToAddPet={handleGoToAddPet} onGoToLogin={handleGoToLogin} onGoToProfile={handleGoToProfile} />}
 
-    if (view === 'profile')
-        return <Profile onGoToHome={handleGoToHome} />
+        {view === 'add-pet' && <AddPet onGoToHome={handleGoToHome} />}
+
+        {view === 'profile' && <Profile onGoToHome={handleGoToHome} />}
+
+    </>
 }
--- staff/agustin-garcia/product/app/views/components/PetList.jsx
+++ staff/agustin-garcia/product/app/views/components/PetList.jsx
@@ -56,27 +56,19 @@ function PetList() {
 
     console.log('PetList -> render')
 
-    const petItems = []
-
-    for (const pet of pets) {
-        const petItem = <li key={pet.id} className="flex items-center justify-between gap-4 mb-2 border-2 border-gray-600 p-2 rounded-md max-w-sm w-full ">
-
-            <div className="flex items-center gap-4">
-                <img src={pet.image} className="rounded-full w-20 h-20 object-cover" />
-
-                <p className="font-bold">{pet.name}</p>
-            </div>
-
-            <button className="justify-self-end cursor-pointer" id={pet.id} onClick={handleDeletePetClick}>🗑️</button>
-        </li>
-
-        petItems.push(petItem)
-    }
-
     return <div>
-        <ul className="flex flex-col gap-2 mt-2">
-            {petItems}
-        </ul>
+        {
+            pets.map(pet => <li key={pet.id} className="flex items-center justify-between gap-4 mb-2 border-2 border-gray-600 p-2 rounded-md max-w-sm w-full ">
+
+                <div className="flex items-center gap-4">
+                    <img src={pet.image} className="rounded-full w-20 h-20 object-cover" />
+
+                    <p className="font-bold">{pet.name}</p>
+                </div>
+
+                <button className="justify-self-end cursor-pointer" id={pet.id} onClick={handleDeletePetClick}>🗑️</button>
+            </li>)
+        }
 
         {showPanel && < div className="w-full h-full fixed top-0 left-0 bg-black/75 flex justify-center items-center panel">
             <div className="bg-white border-black border-2 p-2 rounded-lg">
