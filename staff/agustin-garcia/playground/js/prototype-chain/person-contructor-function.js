0b315ef7248d8cff897c5586d561559642a4a69a
Agustin.dev <Agustin.tech@proton.me>
2026-02-25 18:03:48 +0100

    add prototype-chain inheritance test

---
+++ staff/agustin-garcia/playground/js/prototype-chain/person-contructor-function.js
@@ -0,0 +1,105 @@
+function Person(name, age, gender) {
+    this.name = name
+    this.age = age
+    this.gender = gender
+}
+
+Person.prototype.fart = function () {
+    return '💨'
+}
+
+function Woman(name, age) {
+    Person.call(this, name, age, 'female')
+}
+
+Woman.prototype = Object.create(Person.prototype)
+Woman.prototype.constructor = Woman
+
+Woman.prototype.giveBirth = function () {
+    return '👶'
+}
+
+function StraightWoman(name, age) {
+    Woman.call(this, name, age)
+}
+
+StraightWoman.prototype = Object.create(Woman.prototype)
+StraightWoman.prototype.constructor = StraightWoman
+
+function Lesbian(name, age) {
+    Woman.call(this, name, age)
+}
+
+Lesbian.prototype = Object.create(Woman.prototype)
+Lesbian.prototype.constructor = Lesbian
+
+Woman.prototype.provideOvulus = function () {
+    return '🥚'
+}
+
+function Man(name, age) {
+    Person.call(this, name, age, 'male')
+}
+
+Man.prototype = Object.create(Person.prototype)
+Man.prototype.constructor = Man
+
+Man.prototype.provideSperm = function () {
+    return '💦'
+}
+
+function StraightMan(name, age) {
+    Man.call(this, name, age)
+}
+
+StraightMan.prototype = Object.create(Man.prototype)
+StraightMan.prototype.constructor = StraightMan
+
+function Gay(name, age) {
+    Man.call(this, name, age)
+}
+
+Gay.prototype = Object.create(Man.prototype)
+Gay.prototype.constructor = Gay
+
+function Trans(name, age, gender, operated) {
+    Person.call(this, name, age, gender)
+    this.operated = operated
+
+    if (gender === 'female' && !operated)
+        this.provideOvulus = Woman.prototype.provideOvulus
+    else if (gender === 'male' && !operated)
+        this.provideSperm = Man.prototype.provideSperm
+}
+
+Trans.prototype = Object.create(Person.prototype)
+Trans.prototype.constructor = Trans
+
+let wendy = new StraightWoman('Wendy', 22)
+let peter = new Man('Peter', 23)
+let petra = new Trans('Petra', 23, 'male', true)
+let wendu = new Trans('Wendy', 22, 'female', false)
+let campa = new Lesbian('Campa', 19)
+let pepito = new Gay('Pepito', 40)
+
+console.log('wendy')
+console.log('is woman', wendy instanceof Woman)
+console.log('is man', wendy instanceof Man)
+console.log('is person', wendy instanceof Person)
+
+console.log('peter')
+console.log('is woman', peter instanceof Woman)
+console.log('is man', peter instanceof Man)
+console.log('is person', peter instanceof Person)
+
+console.log('petra')
+console.log('is woman', petra instanceof Woman)
+console.log('is man', petra instanceof Man)
+console.log('is person', petra instanceof Person)
+console.log('is trans', petra instanceof Trans)
+
+console.log('wendy')
+console.log('is woman', wendu instanceof Woman)
+console.log('is man', wendu instanceof Man)
+console.log('is person', wendu instanceof Person)
+console.log('is trans', wendu instanceof Trans)
