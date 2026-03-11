e9928eed4639f1cbe93ed0aa4a0050a666428134
Agustin.dev <Agustin.tech@proton.me>
2026-03-11 19:13:36 +0100

    add mongoose and create user and pet schemas and models; use them in popuplate

--- staff/agustin-garcia/product/api/populate.js
+++ staff/agustin-garcia/product/api/populate.js
@@ -1,44 +1,96 @@
-import { MongoClient, ObjectId } from 'mongodb'
+import mongoose from 'mongoose'
 
-const client = new MongoClient('mongodb://localhost:27017')
+const { Schema, ObjectId, model } = mongoose
 
-client.connect()
-    .then(() => {
-        const db = client.db('product')
+const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const URL_REGEX = /(www|http:|https:)+[^\s]+[\w]/
 
-        const users = db.collection('users')
-        const pets = db.collection('pets')
+const userSchema = new Schema({
+    name: {
+        type: String,
+        minLength: 1,
+        required: true
+    },
+    email: {
+        type: String,
+        minLength: 6,
+        match: EMAIL_REGEX,
+        required: true,
+        unique: true
+    },
 
-        // users.find({}).toArray()
-        //     .then(users => console.table(users))
-        //     .catch(error => console.error(error))
+    username: {
+        type: String,
+        minLength: 3,
+        required: true,
+        unique: true
+    },
 
-        // users.insertOne({name: 'le Chuga', email: 'le@chuga.com', username:'lechuga', password:'123123123' })
-        //     .then(result => console.log(result))
-        //     .catch(error => console.error(error))
+    password: {
+        type: String,
+        minLength: 8,
+        required: true
+    },
 
-        // users.updateOne({ _id: new ObjectId('69af2431eb5f77938c7c290c')}, {$set: {password: '345345345'}})
-        //     .then(result => console.log(result))
-        //     .catch(error => console.error(error))
+    image: {
+        type: String,
+        match: URL_REGEX
+    },
 
-        // users.findOne({ _id: new ObjectId('69af2431eb5f77938c7c290c') })
-        //     .then(user => console.log(user))
-        //     .catch(error => console.error(error))
+    role: {
+        type: String,
+        enum: ['regular', 'administrator'],
+        default: 'regular',
+        required: true
+    }
+})
 
-        // users.find({ name: /l/i }).toArray()
-        //     .then(users => console.table(users))
-        //     .catch(error => console.error(error))
+const petSchema = new Schema({
+    owner: {
+        type: ObjectId
+    },
 
-        // users.deleteOne({ _id: new ObjectId('69af238eeb5f77938c7c290a') })
-        //     .then(result => console.log(result))
-        //     .catch(error => console.error(error))
+    name: {
+        type: String,
+        minLength: 1,
+        required: true
+    },
+    birthdate: {
+        type: Date,
+        required: true
+    },
 
-        // pets.insertOne({ userId: new Object('69af21aaeb5f77938c7c2907'), name: 'rocky', birthdate: new Date('2016-03-22'), weight: 3, image: 'https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3bTYzMzVreTRwaHVpM3BzMWx6NTFyYW53NXlpcmg5c2R3YXU1MHhxNCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/MjOSt1BtJVvM4oDKvI/giphy.gif' })
-        //     .then(result => console.log(result))
-        //     .catch(error => console.error(error))
+    weight: {
+        type: Number,
+        required: true
+    },
 
-        // pets.deleteMany({ userId: new ObjectId('69af2430eb5f77938c7c290b') })
-        //     .then(result => console.log(result))
-        //     .catch(error => console.log(error))
+    image: {
+        type: String,
+        match: URL_REGEX,
+        required: true
+    }
+})
+
+const User = model('User', userSchema)
+const Pet = model('Pet', petSchema)
+
+mongoose.connect('mongodb://localhost:27017/product')
+
+const wendy = new User({ name: 'Wendy Darling', email: 'wendy@darling.com', username: 'wendydarling', password: '123123123' })
+const pepitogrillo = new User({ name: 'Pepito Grillo ', email: 'pepito@grillo.com', username: 'pepitogrillo', password: '123123123' })
+
+Promise.all([wendy.save(), pepitogrillo.save()])
+    .then(([wendy, pepitogrillo]) => {
+        console.log(wendy, pepitogrillo)
+
+        const tor = new Pet({ owner: wendy.id, name: 'Tor', birthdate: new Date('2018-02-05'), weight: 7, image: 'https://media.giphy.com/media/v1.Y2lkPWVjZjA1ZTQ3eWYyeWx6cTlsbzB5eDdoZ2QzNTE1d2EzMnd1NG96Y25rZWwydnoxdyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/c5J5viTR15V7rpcKoM/giphy.gif' })
+
+        const rocky = new Pet({ owner: wendy.id, name: 'Rocky', birthdate: new Date('2021-11-05'), weight: 5, image: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXZnbnJ4ZTY0Mm1oa2Nqb2Uzcnh4cjN3ZW85bmZ4ajFhN3U1Mjh3NCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/j0QzDgFZRX2njRxxtP/giphy.gif' })
+
+        const tommy = new Pet({ owner: pepitogrillo.id, name: 'Tommy', birthdate: new Date('2022-06-08'), weight: 6, image: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGU2dmlncDd4NmE1dXdodHN6czY2MDdtNXl2a2Q2a3hhdmEzNzJ4MCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/OHRF8LZis06OiPDJby/giphy.gif' })
+
+        return Promise.all([tor.save(), rocky.save(), tommy.save()])
     })
+    .then(([tor, rocky]) => console.log(tor, rocky))
     .catch(error => console.error(error))
